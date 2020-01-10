58b075e83d5d11f0ba6457d6874ca2110c9fc963
Fan <15950194@qq.com>
2020-01-10 17:11:45 +0800

    auto-configuration for shared beans

--- stock-client/stock-ui/src/main/java/fan/zheyuan/demo/stockui/ChartController.java
+++ stock-client/stock-ui/src/main/java/fan/zheyuan/demo/stockui/ChartController.java
@@ -1,5 +1,6 @@
 package fan.zheyuan.demo.stockui;
 
+import fan.zheyuan.demo.stockclient.WebClientStockClient;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import org.springframework.stereotype.Component;
@@ -9,4 +10,9 @@ public class ChartController {
 
     @FXML
     public LineChart<String, Double> chart;
+    private WebClientStockClient webClientStockClient;
+
+    public ChartController(WebClientStockClient webClientStockClient) {
+        this.webClientStockClient = webClientStockClient;
+    }
 }

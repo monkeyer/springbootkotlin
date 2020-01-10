5935fc3424e9cb61134cdc74b2a6b6e7531402b7
Fan <15950194@qq.com>
2020-01-10 15:34:35 +0800

    JavaFX Line Chart

--- stock-client/stock-ui/src/main/java/fan/zheyuan/demo/stockui/ChartController.java
+++ stock-client/stock-ui/src/main/java/fan/zheyuan/demo/stockui/ChartController.java
@@ -1,4 +1,12 @@
 package fan.zheyuan.demo.stockui;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ChartController {
+
+    @FXML
+    public LineChart<String, Double> chart;
 }
--- stock-client/stock-ui/src/main/java/fan/zheyuan/demo/stockui/StageInitializer.java
+++ stock-client/stock-ui/src/main/java/fan/zheyuan/demo/stockui/StageInitializer.java
@@ -6,25 +6,36 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.net.URL;
 
 @Component
 public class StageInitializer implements ApplicationListener<StageReadyEvent> {
     @Value("classpath:/chart.fxml")
     private Resource ChartResource;
+    private String applicationTitle;
+    private ApplicationContext applicationContext;
+
+    public StageInitializer(@Value("${spring.application.ui.title}") String applicationTitle,
+                            ApplicationContext applicationContext) {
+        this.applicationTitle = applicationTitle;
+        this.applicationContext = applicationContext;
+    }
 
     @Override
     public void onApplicationEvent(StageReadyEvent stageReadyEvent) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(ChartResource.getURL());
-            Stage stage = stageReadyEvent.getStage();
+            fxmlLoader.setControllerFactory(aClass -> applicationContext.getBean(aClass));
             Parent parent = fxmlLoader.load();
+
+            Stage stage = stageReadyEvent.getStage();
             stage.setScene(new Scene(parent, 800, 600));
+            stage.setTitle(applicationTitle);
             stage.show();
         } catch (IOException e) {
             throw new RuntimeException();

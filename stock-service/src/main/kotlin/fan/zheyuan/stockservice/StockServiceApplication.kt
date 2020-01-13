fdd14fa2a02198a99bd403e7e385e9b1b85790f1
Fan <15950194@qq.com>
2020-01-13 17:31:28 +0800

    kotlin RSocket Server

--- stock-service/src/main/kotlin/fan/zheyuan/stockservice/StockServiceApplication.kt
+++ stock-service/src/main/kotlin/fan/zheyuan/stockservice/StockServiceApplication.kt
@@ -3,8 +3,10 @@ package fan.zheyuan.stockservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.http.MediaType
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.stereotype.Controller
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.Mapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
@@ -21,18 +23,33 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class RestController {
+class RestController(val priceService: PriceService) {
 
     @GetMapping(value = ["/stocks/{symbol}"],
             produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-    fun prices(@PathVariable symbol: String): Flux<StockPrice>  =
-        Flux.interval(Duration.ofSeconds(1))
-                .map { StockPrice(symbol, randomStockPrice(), now()) }
+    fun prices(@PathVariable symbol: String): Flux<StockPrice> =
+            priceService.generatePrices(symbol)
+
+}
+
+@Controller
+class RSocketController(val priceService: PriceService) {
+    @MessageMapping("stockPrices")
+    fun prices(symbol: String): Flux<StockPrice> =
+            priceService.generatePrices(symbol)
+}
+
+@Service
+class PriceService {
+    fun generatePrices(symbol: String): Flux<StockPrice> =
+            Flux.interval(Duration.ofSeconds(1))
+                    .map { StockPrice(symbol, randomStockPrice(), now()) }
 
     private fun randomStockPrice(): Double =
             ThreadLocalRandom.current().nextDouble(100.0)
+
 }
 
-data class StockPrice (val symbol: String,
-                       val price: Double,
-                       val time:LocalDateTime)
+data class StockPrice(val symbol: String,
+                      val price: Double,
+                      val time: LocalDateTime)
